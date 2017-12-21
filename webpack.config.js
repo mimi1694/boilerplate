571fde7106723af0aefc7a83fc5f502151dadbd8
Mimi Scott <mimiscott94@gmail.com>
2017-12-21 15:47:54 -0500

    boilerplate without authentication

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    entry: './client/index.js', // assumes your entry point is the index.js in the root of your project folder
+    output: {
+      path: __dirname,
+      filename: './bundle.js' // assumes your bundle.js will also be in the root of your project folder
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+              test: /\.jsx?$/,
+              exclude: /(node_modules|bower_components)/,
+              loader: 'babel-loader',
+              options: {
+                presets: ['react', 'es2015']
+              }
+            },
+            // use the style-loader/css-loader combos for anything matching the .css extension
+            {
+              test: /\.css$/,
+              use: [
+                'style-loader',
+                'css-loader',
+              ]
+            }
+          ]
+    }
+  };
